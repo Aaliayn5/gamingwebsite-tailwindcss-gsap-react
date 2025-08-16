534edf2744d0466d44ae6ffeb7a0a399384dc3d2
Aaliyan5 <aaliyanahmed24@gmail.com>
2025-08-16 17:36:40 +0300

    Initial commit: Gaming website with Tailwind CSS and video player

---
+++ src/components/hero.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react"
+import { useRef } from "react";
+import Button from "./button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const hero = () => {
+ 
+
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [LoadedVideos, setLoadedVideos] = useState(0);
+    const handleVideoLoaded = () => {
+        setLoadedVideos(prevLoadedVideos => prevLoadedVideos + 1);
+    };
+
+    const totalVideos = 3;
+    const nextVideoRef = useRef(null);
+
+    const upComingVideoIndex = (currentIndex % totalVideos) + 1;
+
+
+
+    const handleMiniVideoPlayer = () => {
+        setHasClicked(true);
+        setCurrentIndex(upComingVideoIndex);
+
+    }
+
+    useGSAP(() => {
+        if (hasClicked) {
+            gsap.set('#next-video' , {visibility: 'visible'});
+
+            gsap.to('#next-video' , {
+               transformOrigin: 'center center',
+               scale: 1.5,
+               width: '100%',
+               height: '100%',
+               duration: 1,
+               ease: 'power2.inOut',
+               onStart: () => nextVideoRef.current.play(),
+            });
+
+            gsap.from('#current-video' , {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power2.inOut',
+
+            })
+        }
+
+    }, {dependencies: [currentIndex], revertOnUpdate: true})
+
+    const getVideoSource = (index) => {
+        return `/videos/hero-${index}.mp4`;
+    }
+
+   
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+        <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+            <div>
+                <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                    <div onClick={handleMiniVideoPlayer} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:scale-100 hover:opacity-100   ">
+                        <video ref={nextVideoRef} src={getVideoSource(upComingVideoIndex)} loop muted id="current-video" className="size-64 origin-center scale-150 object-cover object-center" onLoadedData={handleVideoLoaded} ></video>
+                    </div>
+
+                </div>
+                <video
+            ref={nextVideoRef}      
+            src={getVideoSource(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoaded}
+
+            />
+
+            <video 
+            src={getVideoSource(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover"
+            onLoadedData={handleVideoLoaded}
+            />
+            </div>
+            <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">G<b>a</b>ming</h1>
+            <div className="absolute left-0 top-0 z-40 size-full">
+                <div className="mt-24 px-5 sm:px-10">
+                    <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b><b>e</b>d</h1>
+                    <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br /> unleash the Play Economy</p>
+
+                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1"/>
+
+                </div>
+            </div>
+        </div>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">G<b>a</b>ming</h1>
+    </div>
+  )
+}
+
+export default hero
