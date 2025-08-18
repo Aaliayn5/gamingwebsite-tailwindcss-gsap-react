5e90d118283719072776fb6ac74b3c23add11b78
Aaliyan5 <aaliyanahmed24@gmail.com>
2025-08-18 16:45:20 +0300

    complete web no footer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useRef } from 'react';
 import { TiLocationArrow } from 'react-icons/ti';
 import Button from './button.jsx';
+import { useWindowScroll } from 'react-use';
+import gsap from 'gsap';
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
@@ -11,6 +13,37 @@ const NavBar = () => {
   const navContainerRef = useRef(null);
   const audioElementRef = useRef(null);
   const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+  const [LastScrollY, setLastScrollY] = useState(0);
+  const [isNavVisible, setIsNavVisible] = useState(true);
+
+  const { y: currentScrollY } = useWindowScroll();
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove('floating-nav')
+    } else if (currentScrollY > LastScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add('floating-nav')
+    } else if (currentScrollY < LastScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add('floating-nav')
+    }
+    setLastScrollY(currentScrollY);
+
+  },[currentScrollY]);
+
+  useEffect(() => {
+    gsap.to((navContainerRef.current),{
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    })
+
+  },[isNavVisible])
+
+
+
 
   const toggleAudioIndicator = () => {
     setIsAudioPlaying((prev) => !prev);
@@ -27,7 +60,7 @@ const NavBar = () => {
     
   return (
     <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
-        <header className='absolute top-1/2 w-full -transalte-y-1/2'>
+        <header className='absolute w-full -transalte-y-1/2'>
             <nav className='flex size-full items-center justify-between p-4'>
                 <div className='flex items-center gap-7'>
                     <img src="/img/logo.png" alt="logo" className='w-10' />
